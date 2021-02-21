59697eb36eb8f5e07d29a48744be1249c2bd3467
hongxi <p123hx@gmail.com>
2021-02-20 22:22:23 -0500

    cout dist_mat rm reprodu.

--- embedding/reproducibility.cpp
+++ embedding/reproducibility.cpp
@@ -260,7 +260,7 @@ print_time, double sigma, unsigned window_size
 //         << "Time 3:" << duration3.count() << endl
 //         << "Time 4:" << duration4.count() << endl
 //    cout << "total: " << tout << endl;
-    cout << distance_mat << endl;
+//    cout << distance_mat << endl;
     vector<double> tv = {(double) tout, (double) tout1, (double) tout2,(double)tout3,
                          (double)tout4};
     return tv;
